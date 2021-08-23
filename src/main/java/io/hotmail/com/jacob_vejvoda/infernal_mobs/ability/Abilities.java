f9d1d317b4ec49995a4b62bdcfc287f67183c56c
Siroshun09 <siro.shun8@gmail.com>
2021-08-23 22:39:33 +0900

    refactor: confuse ability

--- src/main/java/io/hotmail/com/jacob_vejvoda/infernal_mobs/ability/Abilities.java
+++ src/main/java/io/hotmail/com/jacob_vejvoda/infernal_mobs/ability/Abilities.java
@@ -6,10 +6,12 @@ import org.jetbrains.annotations.Nullable;
 public final class Abilities {
 
     public static final Ability ONE_UP = new OneUpAbility();
+    public static final Ability CONFUSE = new ConfuseAbility();
 
     public static @Nullable Ability fromName(@NotNull String name) {
         return switch (name) {
             case "1up" -> ONE_UP;
+            case "confuse" -> CONFUSE;
             default -> null;
         };
     }
