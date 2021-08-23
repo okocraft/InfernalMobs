8571824e58a434aa2747e77b98a2a875ce71eb08
Siroshun09 <siro.shun8@gmail.com>
2021-08-23 22:39:33 +0900

    refactor: confuse ability

---
+++ src/main/java/io/hotmail/com/jacob_vejvoda/infernal_mobs/ability/ConfuseAbility.java
@@ -0,0 +1,63 @@
+package io.hotmail.com.jacob_vejvoda.infernal_mobs.ability;
+
+import io.hotmail.com.jacob_vejvoda.infernal_mobs.InfernalMobsPlugin;
+import org.bukkit.Sound;
+import org.bukkit.entity.Mob;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.jetbrains.annotations.NotNull;
+
+public class ConfuseAbility extends AbstractAbility {
+
+    private static final PotionEffect CONFUSION = new PotionEffect(PotionEffectType.CONFUSION, 80, 2);
+
+    ConfuseAbility() {
+        super("confuse");
+    }
+
+    @Override
+    public void onSpawn(@NotNull Mob infernalMob) {
+    }
+
+    @Override
+    public void attack(@NotNull Mob infernalMob, @NotNull Mob target) {
+        if (InfernalMobsPlugin.RANDOM.nextInt(20) == 0) {
+            CONFUSION.apply(target);
+
+            if (target instanceof Player player) {
+                player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_BURP, 100, 0.75f);
+            }
+        }
+    }
+
+    @Override
+    public void counterattack(@NotNull Mob infernalMob, @NotNull Mob target) {
+        if (InfernalMobsPlugin.RANDOM.nextInt(20) == 0) {
+            CONFUSION.apply(target);
+
+            if (target instanceof Player player) {
+                player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_BURP, 100, 0.75f);
+            }
+        }
+    }
+
+    @Override
+    public void showEffect(@NotNull Mob infernalMob) {
+
+    }
+
+    @Override
+    public void onDamaged(@NotNull Mob infernalMob, double damageAmount) {
+
+    }
+
+    @Override
+    public void onDamaged(@NotNull Mob infernalMob, @NotNull Mob damager, double damageAmount) {
+    }
+
+    @Override
+    public void onKilled(@NotNull Mob infernalMob, @NotNull Mob killer) {
+
+    }
+}
