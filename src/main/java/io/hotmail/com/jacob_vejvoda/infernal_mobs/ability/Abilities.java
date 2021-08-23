eaea9937fb98c030ba91fd6d42ee6e67bcd5071c
Siroshun09 <siro.shun8@gmail.com>
2021-08-23 23:09:39 +0900

    refactor: blinding ability

--- src/main/java/io/hotmail/com/jacob_vejvoda/infernal_mobs/ability/Abilities.java
+++ src/main/java/io/hotmail/com/jacob_vejvoda/infernal_mobs/ability/Abilities.java
@@ -6,12 +6,14 @@ import org.jetbrains.annotations.Nullable;
 public final class Abilities {
 
     public static final Ability ONE_UP = new OneUpAbility();
+    public static final Ability BLINDING = new BlindingAbility();
     public static final Ability CONFUSE = new ConfuseAbility();
     public static final Ability WITHERING = new WitheringAbility();
 
     public static @Nullable Ability fromName(@NotNull String name) {
         return switch (name) {
             case "1up" -> ONE_UP;
+            case "blinding" -> BLINDING;
             case "confuse" -> CONFUSE;
             case "withering" -> WITHERING;
             default -> null;
