147c9979a0e07d09496257defc8c1c970b0db74a
Siroshun09 <siro.shun8@gmail.com>
2021-08-23 23:50:03 +0900

    refactor: bulwark ability

---
+++ src/main/java/io/hotmail/com/jacob_vejvoda/infernal_mobs/ability/BulwarkAbility.java
@@ -0,0 +1,40 @@
+package io.hotmail.com.jacob_vejvoda.infernal_mobs.ability;
+
+import io.hotmail.com.jacob_vejvoda.infernal_mobs.InfernalMobsPlugin;
+import org.bukkit.Particle;
+import org.bukkit.entity.Mob;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.jetbrains.annotations.NotNull;
+
+public class BulwarkAbility extends AbstractAbility {
+
+    private static final PotionEffect RESISTANCE = new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 80, 2);
+
+    public BulwarkAbility() {
+        super("bulwark");
+    }
+
+    @Override
+    public void attack(@NotNull Mob infernalMob, @NotNull Mob target) {
+        if (InfernalMobsPlugin.RANDOM.nextInt(20) == 0) {
+            applyEffect(infernalMob);
+        }
+    }
+
+    @Override
+    public void counterattack(@NotNull Mob infernalMob, @NotNull Mob target) {
+        if (InfernalMobsPlugin.RANDOM.nextInt(20) == 0) {
+            applyEffect(infernalMob);
+        }
+    }
+
+    private void applyEffect(@NotNull Mob target) {
+        RESISTANCE.apply(target);
+
+        var world = target.getWorld();
+        var location = target.getLocation();
+
+        world.spawnParticle(Particle.BLOCK_DUST, location.add(0, 1, 0), 100, 0.5, 0.5, 0.5);
+    }
+}
