99b43293b2c5c47e3367ba577e89ae4c9884bcbf
Siroshun09 <siro.shun8@gmail.com>
2021-08-23 20:13:45 +0900

    feat: add NamespaceFactory

---
+++ src/main/java/io/hotmail/com/jacob_vejvoda/infernal_mobs/util/NamespaceFactory.java
@@ -0,0 +1,25 @@
+package io.hotmail.com.jacob_vejvoda.infernal_mobs.util;
+
+import io.hotmail.com.jacob_vejvoda.infernal_mobs.InfernalMobsPlugin;
+import org.bukkit.NamespacedKey;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+public final class NamespaceFactory {
+
+    private static Plugin PLUGIN;
+
+    public static void init(@NotNull InfernalMobsPlugin plugin) {
+        if (PLUGIN != null) {
+            PLUGIN = plugin;
+        }
+    }
+
+    @Contract("_ -> new")
+    public static @NotNull NamespacedKey create(@NotNull String value) {
+        return new NamespacedKey(PLUGIN, value);
+    }
+
+}
