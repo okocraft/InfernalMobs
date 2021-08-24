7bacbbc727f68b137d155f3888595a7b351e9ebf
Siroshun09 <siro.shun8@gmail.com>
2021-08-24 20:03:12 +0900

    refactor: lifesteal ability

--- src/main/java/io/hotmail/com/jacob_vejvoda/infernal_mobs/ability/Abilities.java
+++ src/main/java/io/hotmail/com/jacob_vejvoda/infernal_mobs/ability/Abilities.java
@@ -9,6 +9,7 @@ public final class Abilities {
     public static final Ability BLINDING = new BlindingAbility();
     public static final Ability BULWARK = new BulwarkAbility();
     public static final Ability CONFUSE = new ConfuseAbility();
+    public static final Ability LIFE_STEAL = new LifeSteal();
     public static final Ability QUICKSAND = new QuicksandAbility();
     public static final Ability RUST = new RustAbility();
     public static final Ability SAPPER = new SapperAbility();
@@ -21,6 +22,7 @@ public final class Abilities {
             // bullwark is probably a typo, but it is used in the configuration, so we'll keep it for compatibility...
             case "bulwark", "bullwark" -> BULWARK;
             case "confuse" -> CONFUSE;
+            case "lifesteal" -> LIFE_STEAL;
             case "quicksand" -> QUICKSAND;
             case "rust" -> RUST;
             case "sapper" -> SAPPER;
